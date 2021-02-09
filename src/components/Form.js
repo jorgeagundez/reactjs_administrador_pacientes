2bd780d74d1de05e4724372f351dfa32f031468d
Jorge <jagundez@runroom.com>
2021-02-09 21:26:49 +0100

    Add onChange event to get state of the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,81 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
-const Form = () => <h1>Desde Formulario</h1>;
+const Form = () => {
+
+    // Crear el State de Citas
+    const [cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: '',
+    });
+
+    // Función que se ejecuta cada vez que el usuario escribe en un input
+    const handleChange = event => {
+        actualizarCita({
+            ...cita,
+            [event.target.name] : event.target.value
+        })
+    }
+
+    return (
+        <Fragment>
+            <h2>Crear cita</h2>
+
+            <form>
+                <label>Nombre mascota</label>
+                <input
+                    type="text"
+                    name="mascota"
+                    className="u-full-width"
+                    placeholder="Nombre Mascota"
+                    onChange={handleChange}
+                />
+
+                <label>Nombre del dueño</label>
+                <input
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre dueño de la mascota"
+                    onChange={handleChange}
+                />
+
+                <label>Fecha de alta</label>
+                <input
+                    type="date"
+                    name="fecha"
+                    className="u-full-width"
+                    onChange={handleChange}
+                />
+
+                <label>Hora</label>
+                <input
+                    type="time"
+                    name="hora"
+                    className="u-full-width"
+                    onChange={handleChange}
+                />
+
+                <label>Síntomas</label>
+                <textarea
+                    className="u-full-width"
+                    name="sintomas"
+                    onChange={handleChange}
+                ></textarea>
+
+                <button
+                    type="submit"
+                    className="u-full-width button-primary"
+                >
+                    Agregar Cita
+                </button>
+
+
+            </form>
+        </Fragment>
+    )
+}
 
 export default Form;
