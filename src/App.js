abf5ef8201d9cf7c4ea78cb91a2faa20828b3e69
Jorge <jagundez@runroom.com>
2021-02-13 18:04:46 +0100

    Add conditional title depending on number total of citas

--- src/App.js
+++ src/App.js
@@ -18,9 +18,11 @@ function App() {
   // Función que elimina una cita por su id
   const eliminarCita = id => {
     const nuevasCitas = citas.filter(cita => cita.id !== id);
-
     guardarCitas(nuevasCitas)
   }
+
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas';
 
   return (
     <Fragment>
@@ -34,7 +36,7 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            <h2>Administra tus citas</h2>
+            <h2>{titulo}</h2>
             {citas.map(cita => (
               <Cita
                 key={cita.id}
